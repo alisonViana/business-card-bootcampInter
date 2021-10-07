76a326b139036cdf2ce14af50bc0ce6514836a2c
alisonViana <alisonviana.c@gmail.com>
2021-10-07 20:03:29 -0300

    created project

---
+++ app/src/main/java/br/com/dio/businesscard/App.kt
@@ -0,0 +1,10 @@
+package br.com.dio.businesscard
+
+import android.app.Application
+
+/**
+ * Classe responsável pelo gerenciamento do repositório
+ * Executada antes da MainActivity
+ */
+class App: Application() {
+}
