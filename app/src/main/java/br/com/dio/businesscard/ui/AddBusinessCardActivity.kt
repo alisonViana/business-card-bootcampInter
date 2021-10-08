372c870a81630cf55fee5dece00bad6c798b4a23
alisonViana <alisonviana.c@gmail.com>
2021-10-08 19:43:57 -0300

    refactored layouts

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,6 +2,8 @@ package br.com.dio.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import br.com.dio.businesscard.R
 import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
@@ -18,6 +20,19 @@ class AddBusinessCardActivity : AppCompatActivity() {
     private fun setListeners() {
         binding.btnClose.setOnClickListener {
             finish()
+        }
+
+        binding.btnColorPicker.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+
+            builder.apply {
+                setPositiveButton("Selecionar") { _, _ ->
+                    // faz alguma coisa
+                }
+                setNegativeButton("Cancelar", null)
+                    .setView(R.layout.color_picker_alertdialog)
+                    .show()
+            }
         }
 
         binding.btnConfirm.setOnClickListener {
